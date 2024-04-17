0f291a20a51ffa2c091e013c9fb013272f289b45
jakoismo <shahlarismayilov304@gmail.com>
2024-04-17 14:35:25 +0400

    Cədvəllərim yaradıldı

---
+++ Core/ApiProject.Domain/Common/EntitieBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Domain.Common
+{
+    public class EntitieBase
+    {
+        public int Id { get; set; } 
+        public DateTime CreatedDate { get; set; }= DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+
+    }
+}
