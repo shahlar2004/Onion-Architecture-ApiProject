609551efcfdad7795856828dfe1c694221a820b0
jakoismo <shahlarismayilov304@gmail.com>
2024-05-07 21:12:30 +0400

    Token Proccess started

---
+++ Core/ApiProject.Application/Interfaces/Tokens/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Application.Interfaces.Tokens
+{
+    public interface ITokenService
+    {
+        Task<JwtSecurityToken> CreateToken(User);
+    }
+}
